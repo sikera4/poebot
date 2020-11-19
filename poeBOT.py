a58292543d281b4941072d62058ed7404ac629ed
Andrew <sikera0812@mail.ru>
2020-11-20 01:21:04 +0300

    yo

--- poeBOT.py
+++ poeBOT.py
@@ -26,84 +26,84 @@ def callback_worker(call):
       if call.data == '0':
             url = 'https://horo.mail.ru/prediction/{}/today/'.format(signseng[0])
             response = requests.get(url)
-            soup = BeautifulShop(response.text, 'html.parser')
+            soup = BeautifulSoup(response.text, 'html.parser')
             result = soup.findAll('div', class_ ="article__item article__item_alignment_left article__item_html")
             msg = result[0].text
             bot.send_message(call.message.chat.id, msg)
       elif call.data == '1':
             url = 'https://horo.mail.ru/prediction/{}/today/'.format(signseng[1])
             response = requests.get(url)
-            soup = BeautifulShop(response.text, 'html.parser')
+            soup = BeautifulSoup(response.text, 'html.parser')
             result = soup.findAll('div', class_ ="article__item article__item_alignment_left article__item_html")
             msg = result[0].text
             bot.send_message(call.message.chat.id, msg)
       elif call.data == '2':
             url = 'https://horo.mail.ru/prediction/{}/today/'.format(signseng[2])
             response = requests.get(url)
-            soup = BeautifulShop(response.text, 'html.parser')
+            soup = BeautifulSoup(response.text, 'html.parser')
             result = soup.findAll('div', class_ ="article__item article__item_alignment_left article__item_html")
             msg = result[0].text
             bot.send_message(call.message.chat.id, msg)
       elif call.data == '3':
             url = 'https://horo.mail.ru/prediction/{}/today/'.format(signseng[3])
             response = requests.get(url)
-            soup = BeautifulShop(response.text, 'html.parser')
+            soup = BeautifulSoup(response.text, 'html.parser')
             result = soup.findAll('div', class_ ="article__item article__item_alignment_left article__item_html")
             msg = result[0].text
             bot.send_message(call.message.chat.id, msg)
       elif call.data == '4':
             url = 'https://horo.mail.ru/prediction/{}/today/'.format(signseng[4])
             response = requests.get(url)
-            soup = BeautifulShop(response.text, 'html.parser')
+            soup = BeautifulSoup(response.text, 'html.parser')
             result = soup.findAll('div', class_ ="article__item article__item_alignment_left article__item_html")
             msg = result[0].text
             bot.send_message(call.message.chat.id, msg)
       elif call.data == '5':
             url = 'https://horo.mail.ru/prediction/{}/today/'.format(signseng[5])
             response = requests.get(url)
-            soup = BeautifulShop(response.text, 'html.parser')
+            soup = BeautifulSoup(response.text, 'html.parser')
             result = soup.findAll('div', class_ ="article__item article__item_alignment_left article__item_html")
             msg = result[0].text
             bot.send_message(call.message.chat.id, msg)
       elif call.data == '6':
             url = 'https://horo.mail.ru/prediction/{}/today/'.format(signseng[6])
             response = requests.get(url)
-            soup = BeautifulShop(response.text, 'html.parser')
+            soup = BeautifulSoup(response.text, 'html.parser')
             result = soup.findAll('div', class_ ="article__item article__item_alignment_left article__item_html")
             msg = result[0].text
             bot.send_message(call.message.chat.id, msg)
       elif call.data == '7':
             url = 'https://horo.mail.ru/prediction/{}/today/'.format(signseng[7])
             response = requests.get(url)
-            soup = BeautifulShop(response.text, 'html.parser')
+            soup = BeautifulSoup(response.text, 'html.parser')
             result = soup.findAll('div', class_ ="article__item article__item_alignment_left article__item_html")
             msg = result[0].text
             bot.send_message(call.message.chat.id, msg)
       elif call.data == '8':
             url = 'https://horo.mail.ru/prediction/{}/today/'.format(signseng[8])
             response = requests.get(url)
-            soup = BeautifulShop(response.text, 'html.parser')
+            soup = BeautifulSoup(response.text, 'html.parser')
             result = soup.findAll('div', class_ ="article__item article__item_alignment_left article__item_html")
             msg = result[0].text
             bot.send_message(call.message.chat.id, msg)
       elif call.data == '9':
             url = 'https://horo.mail.ru/prediction/{}/today/'.format(signseng[9])
             response = requests.get(url)
-            soup = BeautifulShop(response.text, 'html.parser')
+            soup = BeautifulSoup(response.text, 'html.parser')
             result = soup.findAll('div', class_ ="article__item article__item_alignment_left article__item_html")
             msg = result[0].text
             bot.send_message(call.message.chat.id, msg)
       elif call.data == '10':
             url = 'https://horo.mail.ru/prediction/{}/today/'.format(signseng[10])
             response = requests.get(url)
-            soup = BeautifulShop(response.text, 'html.parser')
+            soup = BeautifulSoup(response.text, 'html.parser')
             result = soup.findAll('div', class_ ="article__item article__item_alignment_left article__item_html")
             msg = result[0].text
             bot.send_message(call.message.chat.id, msg)
       elif call.data == '11':
             url = 'https://horo.mail.ru/prediction/{}/today/'.format(signseng[11])
             response = requests.get(url)
-            soup = BeautifulShop(response.text, 'html.parser')
+            soup = BeautifulSoup(response.text, 'html.parser')
             result = soup.findAll('div', class_ ="article__item article__item_alignment_left article__item_html")
             msg = result[0].text
             bot.send_message(call.message.chat.id, msg)
