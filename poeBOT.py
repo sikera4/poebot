2f3aec28a3888f2dabecec131369b90a82019e31
Andrew <sikera0812@mail.ru>
2020-11-20 01:04:59 +0300

    proper horo

--- poeBOT.py
+++ poeBOT.py
@@ -1,12 +1,11 @@
 import telebot
 import random
+import requests
 from telebot import types
 bot = telebot.TeleBot('1481033988:AAGT5qgfAJa-gzdHRztNkdxH_5HQ7YDFz4c')
-first = ["Сегодня — идеальный день для новых начинаний.","Оптимальный день для того, чтобы решиться на смелый поступок!","Будьте осторожны, сегодня звёзды могут повлиять на ваше финансовое состояние.","Лучшее время для того, чтобы начать новые отношения или разобраться со старыми.","Плодотворный день для того, чтобы разобраться с накопившимися делами."] 
-second = ["Но помните, что даже в этом случае нужно не забывать про","Если поедете за город, заранее подумайте про","Те, кто сегодня нацелен выполнить множество дел, должны помнить про","Если у вас упадок сил, обратите внимание на","Помните, что мысли материальны, а значит вам в течение дня нужно постоянно думать про"] 
-second_add = ["отношения с друзьями и близкими.","работу и деловые вопросы, которые могут так некстати помешать планам.","себя и своё здоровье, иначе к вечеру возможен полный раздрай.","бытовые вопросы — особенно те, которые вы не доделали вчера.","отдых, чтобы не превратить себя в загнанную лошадь в конце месяца."] 
-third = ["Злые языки могут говорить вам обратное, но сегодня их слушать не нужно.","Знайте, что успех благоволит только настойчивым, поэтому посвятите этот день воспитанию духа.","Даже если вы не сможете уменьшить влияние ретроградного Меркурия, то хотя бы доведите дела до конца.","Не нужно бояться одиноких встреч — сегодня то самое время, когда они значат многое.","Если встретите незнакомца на пути — проявите участие, и тогда эта встреча посулит вам приятные хлопоты."]
 signs = ['Овен', 'Телец', 'Близнецы', 'Рак', 'Лев', 'Дева', 'Весы', 'Скорпион', 'Стрелец', 'Козерог', 'Водолей', 'Рыбы']
+signseng = ['aries', 'taurus', 'gemini', 'cancer', 'leo', 'virgo', 'libra', 'scorpio', 'sagittarius', 'capricorn', 'aquarius', 'pisces']
+
 @bot.message_handler(commands=['start', 'help'])
 def handle_start_help(message):
       bot.send_message(message.from_user.id, text='Напиши привет!')
@@ -16,16 +15,87 @@ def get_text_messages(message):
         bot.send_message(message.from_user.id, "Привет, сейчас я расскажу тебе гороскоп на сегодня.")
         keyboard = types.InlineKeyboardMarkup()
         for i in range(len(signs) - 1):
-              keyboard.add(types.InlineKeyboardButton(text = signs[i], callback_data = 'zodiac'))
+              keyboard.add(types.InlineKeyboardButton(text = signs[i], callback_data = i))
         bot.send_message(message.from_user.id, text='Выбери свой знак зодиака', reply_markup=keyboard)
   else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
-  if call.data == "zodiac": 
-    msg = random.choice(first) + ' ' + random.choice(second) + ' ' + random.choice(second_add) + ' ' + random.choice(third)
-    bot.send_message(call.message.chat.id, msg)
+      if call.data == 0:
+            url = 'https://horo.mail.ru/prediction/{}/today/'.format(signseng[call.data])
+            response = requests.get(url)
+            soup = BeautifulShop(response.text, 'html.parser')
+            result = soup.findAll('div', class_ ="article__item article__item_alignment_left article__item_html")
+            msg = result[0].text
+      elif call.data == 1:
+            url = 'https://horo.mail.ru/prediction/{}/today/'.format(signseng[call.data])
+            response = requests.get(url)
+            soup = BeautifulShop(response.text, 'html.parser')
+            result = soup.findAll('div', class_ ="article__item article__item_alignment_left article__item_html")
+            msg = result[0].text
+      elif call.data == 2:
+            url = 'https://horo.mail.ru/prediction/{}/today/'.format(signseng[call.data])
+            response = requests.get(url)
+            soup = BeautifulShop(response.text, 'html.parser')
+            result = soup.findAll('div', class_ ="article__item article__item_alignment_left article__item_html")
+            msg = result[0].text
+      elif call.data == 3:
+            url = 'https://horo.mail.ru/prediction/{}/today/'.format(signseng[call.data])
+            response = requests.get(url)
+            soup = BeautifulShop(response.text, 'html.parser')
+            result = soup.findAll('div', class_ ="article__item article__item_alignment_left article__item_html")
+            msg = result[0].text
+      elif call.data == 4:
+            url = 'https://horo.mail.ru/prediction/{}/today/'.format(signseng[call.data])
+            response = requests.get(url)
+            soup = BeautifulShop(response.text, 'html.parser')
+            result = soup.findAll('div', class_ ="article__item article__item_alignment_left article__item_html")
+            msg = result[0].text
+      elif call.data == 5:
+            url = 'https://horo.mail.ru/prediction/{}/today/'.format(signseng[call.data])
+            response = requests.get(url)
+            soup = BeautifulShop(response.text, 'html.parser')
+            result = soup.findAll('div', class_ ="article__item article__item_alignment_left article__item_html")
+            msg = result[0].text
+      elif call.data == 6:
+            url = 'https://horo.mail.ru/prediction/{}/today/'.format(signseng[call.data])
+            response = requests.get(url)
+            soup = BeautifulShop(response.text, 'html.parser')
+            result = soup.findAll('div', class_ ="article__item article__item_alignment_left article__item_html")
+            msg = result[0].text
+      elif call.data == 7:
+            url = 'https://horo.mail.ru/prediction/{}/today/'.format(signseng[call.data])
+            response = requests.get(url)
+            soup = BeautifulShop(response.text, 'html.parser')
+            result = soup.findAll('div', class_ ="article__item article__item_alignment_left article__item_html")
+            msg = result[0].text
+      elif call.data == 8:
+            url = 'https://horo.mail.ru/prediction/{}/today/'.format(signseng[call.data])
+            response = requests.get(url)
+            soup = BeautifulShop(response.text, 'html.parser')
+            result = soup.findAll('div', class_ ="article__item article__item_alignment_left article__item_html")
+            msg = result[0].text
+      elif call.data == 9:
+            url = 'https://horo.mail.ru/prediction/{}/today/'.format(signseng[call.data])
+            response = requests.get(url)
+            soup = BeautifulShop(response.text, 'html.parser')
+            result = soup.findAll('div', class_ ="article__item article__item_alignment_left article__item_html")
+            msg = result[0].text
+      elif call.data == 10:
+            url = 'https://horo.mail.ru/prediction/{}/today/'.format(signseng[call.data])
+            response = requests.get(url)
+            soup = BeautifulShop(response.text, 'html.parser')
+            result = soup.findAll('div', class_ ="article__item article__item_alignment_left article__item_html")
+            msg = result[0].text
+      elif call.data == 11:
+            url = 'https://horo.mail.ru/prediction/{}/today/'.format(signseng[call.data])
+            response = requests.get(url)
+            soup = BeautifulShop(response.text, 'html.parser')
+            result = soup.findAll('div', class_ ="article__item article__item_alignment_left article__item_html")
+            msg = result[0].text
+      bot.send_message(call.message.chat.id, msg)
+
 bot.polling(none_stop=True, interval=0, timeout=45)
 
 
